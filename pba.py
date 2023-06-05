a4845acba69140c582956361cb638e8084044942
BojayJaya <95891943+BojayJaya@users.noreply.github.com>
2023-06-05 10:20:06 +0700

    Update pba.py

--- pba.py
+++ pba.py
@@ -34,13 +34,12 @@ st.set_page_config(
 st.write("""
 <center><h2 style = "text-align: justify;">ANALISIS SENTIMEN PADA WISATA DIENG DENGAN ALGORITMA K-NEAREST NEIGHBOR (K-NN)</h2></center>
 """,unsafe_allow_html=True)
-st.write("### Dosen Pengampu : Dr. FIKA HASTARITA RACHMAN, ST., M.Eng")
 
 with st.container():
     with st.sidebar:
         selected = option_menu(
         st.write("""<h3 style = "text-align: center;"><img src="https://cdn-icons-png.flaticon.com/512/1998/1998664.png" width="120" height="120"></h3>""",unsafe_allow_html=True), 
-        ["Home","Dataset", "Implementation", "Tentang Kami"], 
+        ["Home","Dataset", "Penjelasan Preprocessing", "Penjelasan Tf-idf", "Implementation", "Tentang Kami"], 
             icons=['house', 'bar-chart', 'check2-square', 'person'], menu_icon="cast", default_index=0,
             styles={
                 "container": {"padding": "0!important", "background-color": "#412a7a"},
@@ -82,6 +81,64 @@ with st.container():
         df = pd.read_csv("hasil_preprocessing.csv")
         # df = df.drop(columns=['nama','sentiment','score'])
         st.write(df)
+    elif selected == "Penjelasan Preprocessing":
+        st.write("""###### Penjelasan Prepocessing Data : """)
+        st.write("""1. Case Folding :
+        
+        Case folding adalah proses dalam pemrosesan teks yang mengubah semua huruf dalam teks menjadi huruf kecil atau huruf besar. Tujuan dari case folding adalah untuk mengurangi variasi yang disebabkan oleh perbedaan huruf besar dan kecil dalam teks, sehingga mempermudah pemrosesan teks secara konsisten.
+        
+        Dalam case folding, biasanya semua huruf dalam teks dikonversi menjadi huruf kecil dengan menggunakan metode seperti lowercasing. Dengan demikian, perbedaan antara huruf besar dan huruf kecil tidak lagi diperhatikan dalam analisis teks, sehingga memungkinkan untuk mendapatkan hasil yang lebih konsisten dan mengurangi kompleksitas dalam pemrosesan teks.
+        """)
+        st.write("""2. Tokenize :
+
+        Tokenisasi adalah proses pemisahan teks menjadi unit-unit yang lebih kecil yang disebut token. Token dapat berupa kata, frasa, atau simbol lainnya, tergantung pada tujuan dan aturan tokenisasi yang digunakan.
+
+        Tujuan utama tokenisasi dalam pemrosesan bahasa alami (Natural Language Processing/NLP) adalah untuk memecah teks menjadi unit-unit yang lebih kecil agar dapat diolah lebih lanjut, misalnya dalam analisis teks, pembentukan model bahasa, atau klasifikasi teks.
+        """)
+        st.write("""3. Filtering (Stopword Removal) :
+
+        Filtering atau Stopword Removal adalah proses penghapusan kata-kata yang dianggap tidak memiliki makna atau kontribusi yang signifikan dalam analisis teks. Kata-kata tersebut disebut sebagai stop words atau stopwords.
+
+        Stopwords biasanya terdiri dari kata-kata umum seperti “a”, “an”, “the”, “is”, “in”, “on”, “and”, “or”, dll. Kata-kata ini sering muncul dalam teks namun memiliki sedikit kontribusi dalam pemahaman konten atau pengambilan informasi penting dari teks.
+
+        Tujuan dari Filtering atau Stopword Removal adalah untuk membersihkan teks dari kata-kata yang tidak penting sehingga fokus dapat diarahkan pada kata-kata kunci yang lebih informatif dalam analisis teks. Dengan menghapus stopwords, kita dapat mengurangi dimensi data, meningkatkan efisiensi pemrosesan, dan memperbaiki kualitas hasil analisis.
+        """)
+        st.write("""4. Stemming :
+
+        Stemming dalam pemrosesan bahasa alami (Natural Language Processing/NLP) adalah proses mengubah kata ke dalam bentuk dasarnya atau bentuk kata yang lebih sederhana, yang disebut sebagai “stem”. Stemming bertujuan untuk menghapus infleksi atau imbuhan pada kata sehingga kata-kata yang memiliki akar kata yang sama dapat diidentifikasi sebagai bentuk yang setara.
+        """)
+    elif selected == "Penjelasan Tf-idf":
+        st.write("""###### Penjelasan Ekstraksi Fitur : """)
+        st.write("""TF-IDF :""")
+        st.write("""Ditahap akhir dari text preprocessing adalah term-weighting .Term-weighting merupakan proses pemberian bobot term pada dokumen. Pembobotan ini digunakan nantinya oleh algoritma Machine Learning untuk klasifikasi dokumen. Ada beberapa metode yang dapat digunakan, salah satunya adalah TF-IDF (Term Frequency-Inverse Document Frequency).""")
+        st.write("""TF (Term Frequency) :""")
+        st.write("""TF (Term Frequency) adalah ukuran yang menggambarkan seberapa sering sebuah kata muncul dalam suatu dokumen. Menghitung TF melibatkan perbandingan jumlah kemunculan kata dengan jumlah kata keseluruhan dalam dokumen.""")
+        st.write("""Perhitungan TF (Term Frequency) :
+        
+        TF(term) = (Jumlah kemunculan term dalam dokumen) / (Jumlah kata dalam dokumen)
+        """)
+        st.write("""DF (Document Frequency) :""")
+        st.write("""DF (Document Frequency) adalah ukuran yang menggambarkan seberapa sering sebuah kata muncul dalam seluruh koleksi dokumen. DF menghitung jumlah dokumen yang mengandung kata tersebut.""")
+        st.write("""Perhitungan DF (Document Frequency) :
+        
+        DF(term) = Jumlah dokumen yang mengandung term
+        """)
+        st.write("""IDF (Inverse Document Frequency) :""")
+        st.write("""IDF (Inverse Document Frequency) adalah ukuran yang menggambarkan seberapa penting sebuah kata dalam seluruh koleksi dokumen. IDF dihitung dengan mengambil logaritma terbalik dari rasio total dokumen dengan jumlah dokumen yang mengandung kata tersebut. Tujuan IDF adalah memberikan bobot yang lebih besar pada kata-kata yang jarang muncul dalam seluruh koleksi dokumen.""")
+        st.write("""Perhitungan IDF (Inverse Document Frequency) :
+        
+        IDF(term) = log((Total jumlah dokumen) / (DF(term)))
+        """)
+        st.write("""TF-IDF (Term Frequency-Inverse Document Frequency) :""")
+        st.write("""TF-IDF (Term Frequency-Inverse Document Frequency) adalah metode yang menggabungkan informasi TF dan IDF. TF-IDF memberikan bobot yang lebih tinggi pada kata-kata yang sering muncul dalam dokumen tertentu (TF tinggi) dan jarang muncul dalam seluruh koleksi dokumen (IDF tinggi). Metode ini digunakan untuk mengevaluasi kepentingan relatif suatu kata dalam konteks dokumen.""")
+        st.write("""Perhitungan TF-IDF (Term Frequency-Inverse Document Frequency) :
+        
+        TF-IDF(term, document) = TF(term, document) * IDF(term)
+        """)
+        st.write("""Dalam perhitungan TF-IDF, TF(term, document) adalah nilai TF untuk term dalam dokumen tertentu, dan IDF(term) adalah nilai IDF untuk term di seluruh koleksi dokumen.""")
+        st.write("""Mengubah representasi teks ke dalam vektor
+        """)
+
     elif selected == "Implementation":
         #Getting input from user
         iu = st.text_area('Masukkan kata yang akan di analisa :')
@@ -184,8 +241,9 @@ with st.container():
                 st.error('Negative')
 
     elif selected == "Tentang Kami":
-        st.write("##### Mata Kuliah = Pemrosesan Bahasa Alami - A") 
+        st.write("##### Mata Kuliah = Pembelajaran Mesin - B") 
         st.write('##### Kelompok 5')
         st.write("1. Hambali Fitrianto - 200411100074")
         st.write("2. Pramudya Dwi Febrianto - 200411100042")
         st.write("3. Febrian Achmad Syahputra - 200411100106")
+        
